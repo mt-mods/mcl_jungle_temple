b7b335a6f1cb8ab53e7af99c4316ac1cfca48d67
PrairieWind <prairie.astronomer1@gmail.com>
2023-05-15 13:04:24 -0600

    Add middle floor and temporary loot table

--- init.lua
+++ init.lua
@@ -57,6 +57,7 @@ mcl_structures.register_structure("jungle_temple",{
 		local stonebricktopslab = minetest.find_nodes_in_area(p1,p2,{"mcl_stairs:slab_stonebrick_top"})
 		local stonebrickwall = minetest.find_nodes_in_area(p1,p2,{"mcl_walls:stonebrick_0"})
 		local cobble = minetest.find_nodes_in_area(p1,p2,{"mcl_core:cobble"})
+		local torches = minetest.find_nodes_in_area(p1,p2,{"mcl_torches:torch_wall"})
 
 		-- Grab the positions for the stairs to be replaced.
 		local sbs = get_replacements(stonebrickstair,70,pr)
@@ -64,10 +65,12 @@ mcl_structures.register_structure("jungle_temple",{
 
 		-- Set the nodes into the mossy version.
 		minetest.bulk_set_node(get_replacements(stonebrick,70,pr),{name="mcl_core:stonebrickmossy"})
+		minetest.bulk_set_node(get_replacements(stonebrick,10,pr),{name="mcl_core:stonebrickcracked"})
 		minetest.bulk_set_node(get_replacements(stonebrickslab,70,pr),{name="mcl_stairs:slab_stonebrickmossy" })
 		minetest.bulk_set_node(get_replacements(stonebricktopslab,70,pr),{name="mcl_stairs:slab_stonebrickmossy_top"})
 		minetest.bulk_set_node(get_replacements(stonebrickwall,70,pr),{name="mcl_walls:stonebrickmossy_0"})
 		minetest.bulk_set_node(get_replacements(cobble,70,pr),{name="mcl_core:mossycobble"})
+		minetest.bulk_set_node(get_replacements(torches,30,pr),{name="air"})
 
 		for _, stairs in pairs(sbs) do
 			local stair = minetest.get_node(stairs)
@@ -78,4 +81,30 @@ mcl_structures.register_structure("jungle_temple",{
 			minetest.set_node(stairso, {name="mcl_stairs:stair_stonebrickmossy_outer", param2=stair.param2})
 		end
 	end,
+	daughters = {{
+		files = {modpath.."/schematics/mcl_jungle_temple_middle_floor.mts"},
+			pos = vector.new(0, -7, 0),
+	}},
+	loot = {
+		["mcl_barrels:barrel_closed" ] ={{
+			stacks_min = 2,
+			stacks_max = 6,
+			items = {
+				{ itemstring = "mcl_mobitems:bone", weight = 20, amount_min = 4, amount_max=6 },
+				{ itemstring = "mcl_mobitems:rotten_flesh", weight = 16, amount_min = 3, amount_max=7 },
+				{ itemstring = "mcl_core:gold_ingot", weight = 15, amount_min = 2, amount_max = 7 },
+				{ itemstring = "mcl_core:iron_ingot", weight = 15, amount_min = 1, amount_max = 5 },
+				{ itemstring = "mcl_core:diamond", weight = 3, amount_min = 1, amount_max = 3 },
+				{ itemstring = "mcl_mobitems:saddle", weight = 3, },
+				{ itemstring = "mcl_core:emerald", weight = 2, amount_min = 1, amount_max = 3 },
+				{ itemstring = "mcl_books:book", weight = 1, func = function(stack, pr)
+					mcl_enchanting.enchant_uniform_randomly(stack, {"soul_speed"}, pr)
+				end },
+				{ itemstring = "mcl_mobitems:iron_horse_armor", weight = 1, },
+				{ itemstring = "mcl_mobitems:gold_horse_armor", weight = 1, },
+				{ itemstring = "mcl_mobitems:diamond_horse_armor", weight = 1, },
+				{ itemstring = "mcl_core:apple_gold_enchanted", weight = 2, },
+			}
+		}}
+	}
 })
